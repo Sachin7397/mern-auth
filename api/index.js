850b43890bd592bb9b0b01e12a72bf1f5c345568
Sachin7397 <sachinskamble0910@gmail.com>
2024-07-08 00:21:10 +0530

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,4 +1,14 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+//password contains @ so %40 is hex code
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Error connecting to MongoDB:", err));
 
 const app = express();
 
