efe667726db02feab821c7ab9ee5ee3dbd38d894
Sachin7397 <sachinskamble0910@gmail.com>
2024-07-08 20:47:48 +0530

    create test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
 dotenv.config();
 
 //password contains @ so %40 is hex code
@@ -15,3 +16,5 @@ const app = express();
 app.listen(3000, () => {
   console.log("Sever listening on port 3000!");
 });
+
+app.use("/api/user", userRoutes);
